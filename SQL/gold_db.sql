6fa716c3ef271d517e1d08b0819c77c2a06622a0
Amal A <62348008+Atherius@users.noreply.github.com>
2025-04-05 17:49:23 +0530

    Update gold_db.sql

--- SQL/gold_db.sql
+++ SQL/gold_db.sql
@@ -1,32 +1,26 @@
-use AdventureWorks2022
+use gold_db;
 
-select*from HumanResources.Employee
-select*from HumanResources.EmployeeDepartmentHistory
-select*from HumanResources.EmployeePayHistory
-select*from Person.Person
---BusinessEntityID
-select*from HumanResources.Department
---DepartmentID
---Find the first middle and last name of emp as full name 
---along with their sal whode sal is greater then the avg of their sal
+CREATE TABLE gold_db.transportation_gold AS 
+SELECT 
+    route_name,
+    COUNT(*) AS total_deliveries,
+    AVG(delivery_time) AS avg_delivery_time,
+    AVG(fuel_consumed) AS avg_fuel_consumed,
+    vehicle_id,
+    SUM(distance_covered) AS total_distance,
+    (SUM(distance_covered) / SUM(fuel_consumed)) AS fuel_efficiency,
+    driver_name,
+    delivery_status,
+    COUNT(DISTINCT delivery_id) AS total_deliveries_by_driver,
+    AVG(rating) AS driver_rating,
+    CURDATE() AS report_date
+FROM silver_db.delivery_data_silver
+GROUP BY route_name, vehicle_id, driver_name, delivery_status;
 
-select Full_Name,Salary,Avg_Salary from
-(
-select CONCAT_WS(' ',p.FirstName,p.MiddleName,p.LastName)as Full_Name,
-h.Rate as Salary,
-avg(h.Rate) over(partition by h.BusinessEntityID)as Avg_Salary
-from Person.Person p
-join HumanResources.EmployeePayHistory h
-on p.BusinessEntityID=h.BusinessEntityID
-)as dada
-where Salary>Avg_Salary
+select*from  transportation_gold;
 
-create view loni as
-select CONCAT_WS(' ',p.FirstName,p.MiddleName,p.LastName)as Full_Name,
-h.Rate as Salary,
-avg(h.Rate) over(partition by h.BusinessEntityID)as Avg_Salary
-from Person.Person p
-join HumanResources.EmployeePayHistory h
-on p.BusinessEntityID=h.BusinessEntityID
+drop table transportation_gold;
 
-select *from loni where Salary>Avg_Salary
+select *from silver_db.delivery_data_silver;
+
+desc gold_db.transportation_gold;
